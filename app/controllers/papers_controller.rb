d092020067d62ebaeb0ac6269db3ceba3994241a
Usame Algan <usame.algan@student.hpi.uni-potsdam.de>
2017-11-09 15:14:16 +0100

    editing a paper saves changes. closes #23

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -27,6 +27,16 @@ class PapersController < ApplicationController
 
   end
 
+  def update
+    @paper = Paper.find(params[:id])
+
+    if @paper.update(paper_params)
+      redirect_to @paper
+    else
+      render 'edit'
+    end
+  end
+
   private
 
     def paper_params
