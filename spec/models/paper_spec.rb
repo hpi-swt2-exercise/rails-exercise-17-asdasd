eef027ddc4c8e0ef0af8681036db485f18be87ba
Usame Algan <usame.algan@student.hpi.uni-potsdam.de>
2017-11-09 15:46:59 +0100

    papers can have authors now. closes #27

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe Paper, type: :model do
 
   it "should have an empty list of authors" do
+
     paper = Paper.new
-    expect(paper.authors).to be_nil
+    expect(paper.authors).to be_empty
+
   end
 
 end
